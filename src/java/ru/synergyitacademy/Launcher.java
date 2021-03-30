4c2b429d82e2eba72c05c0a49c4027654a8dbf4c
Kurt Gesse <kamrad.colonel@gmail.com>
2021-03-30 13:35:59 +0400

    upd

--- src/java/ru/synergyitacademy/Launcher.java
+++ src/java/ru/synergyitacademy/Launcher.java
@@ -9,8 +9,6 @@ public class Launcher {
      * @throws Exception список исключений, которые генерируются во время выполнения программы
      */
     public static void main(String[] args) throws Exception {
-        //TODO: переписать данный код
-        // Выведется в консоль фраза Привет, мир!
-        System.out.println("Приветствую слушателей из набора 03.2021!");
+        System.out.println("Привет, мир!");
     }
 }
